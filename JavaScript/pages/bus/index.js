6f51194d0c913b369a8271054c6a80614fd954a6
zhouy <howfar.vip@qq.com>
2019-07-31 18:15:37 +0800

    1

--- JavaScript/pages/bus/index.js
+++ JavaScript/pages/bus/index.js
@@ -10,6 +10,7 @@ Component({
     searchIcon(e) {
       wx.request({
         url:'http://bus.wuhancloud.cn:9087/website//web/420100/search.do?keyword=576&type=line',
+        method:"POST",
         success(res){
           console.log(res)
         },
